4c21d9b5f856a4135e0f0767cace7af658a2e6d2
Brent Latsch <brent.latsch@student.kdg.be>
2022-04-04 16:55:43 +0200

    init commit with project setup

---
+++ Poc.Mediatr.Backend/Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureApplicationLayer(this IServiceCollection services)
+    {
+        ConfigureMediatr(services);
+    }
+
+    private static void ConfigureMediatr(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Poc.Mediatr.Backend/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration["MediatrPocDb"];
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException("No connection string found, unable to connect to the database");
+        }
+
+        services.AddDbContext<MediatrPocDbContext>(
+                options => options.UseSqlServer(connectionString)
+            );
+    }
+}
